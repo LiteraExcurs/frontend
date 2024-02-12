d868ec98589d74871d052dddc419e06e54668e0f
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2024-02-12 23:34:56 +0300

    added bckgroung picture

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,14 +2,14 @@ import styles from './not-found.module.scss';
 import Link from "next/link";
 export default function Custom404() {
     return (
-        <div className={styles.page}>
-            <div className={styles.container}>
-                <h3 className={styles.header}>СТРАНИЦА НЕ НАЙДЕНА</h3>
-                <p className={styles.text}>
-                    Страница, на которую вы пытаетесь попасть не существует, или была удалена.
-                    Перейдите на <Link href={"/"}/>
-                </p>
+            <div className={styles.page}>
+                <div className={styles.container}>
+                    <h3 className={styles.header}>СТРАНИЦА НЕ НАЙДЕНА</h3>
+                    <p className={styles.text}>
+                        Страница, на которую вы пытаетесь попасть не существует, или была удалена.
+                        Перейдите на <Link href={"/"}/>
+                    </p>
+                </div>
             </div>
-        </div>
     )
 }
