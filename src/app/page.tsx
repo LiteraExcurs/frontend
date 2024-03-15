c7a71f4ecdf6d9971f9cf9eb4e50921bfaa53e25
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2024-03-15 22:40:50 +0300

    Add suspense for showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-'use client'
+'use client';
+
+import { Suspense } from 'react';
 import { MainArticle } from '@/components/main-article';
 import { Showcase } from '@/components/showcase';
 import { Guides } from '@/components/guides';
+import { Placeholder } from '@/components/placeholder/placeholder';
 import { Provider } from 'react-redux';
 import { store } from '@/services/store';
 
 export default function Home() {
   return (
     <>
-    {/* <Provider store={store}> */}
-        <MainArticle />
+      {/* <Provider store={store}> */}
+      <MainArticle />
+      <Suspense fallback={<Placeholder />}>
         <Showcase />
-        <Guides />
+      </Suspense>
+      <Guides />
       {/* </Provider> */}
     </>
   );
-};
+}
